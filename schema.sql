d7f3ea23c02bc00114faf90a08d6eba620d09f75
Harufork <Harufork@users.noreply.github.com>
2020-09-26 16:40:37 +0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,29 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1:3306
--- Время создания: Сен 26 2020 г., 14:13
--- Версия сервера: 5.7.25
--- Версия PHP: 7.3.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `doisgone`
---
-
--- --------------------------------------------------------
-
 --
 -- Структура таблицы `project`
 --
@@ -176,10 +154,6 @@ INSERT INTO `user` (`ID`, `Email`, `Name`, `RegistrationDate`, `Password`) VALUE
 (2, 'ladybag2001@gmail.com', 'Сергей', '2007-09-16', '2');
 
 --
--- Индексы сохранённых таблиц
---
-
---
 -- Индексы таблицы `project`
 --
 ALTER TABLE `project`
@@ -202,9 +176,6 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `Email` (`Email`);
 
---
--- AUTO_INCREMENT для сохранённых таблиц
---
 
 --
 -- AUTO_INCREMENT для таблицы `project`
@@ -225,10 +196,6 @@ ALTER TABLE `user`
   MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- Ограничения внешнего ключа сохраненных таблиц
---
-
---
 -- Ограничения внешнего ключа таблицы `project`
 --
 ALTER TABLE `project`
@@ -242,6 +209,3 @@ ALTER TABLE `task`
   ADD CONSTRAINT `task_ibfk_2` FOREIGN KEY (`Project`) REFERENCES `project` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
